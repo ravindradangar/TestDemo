e9d4651b87563b20551274e68d93b2006e940080
RRD <ravindra.dangar@rku.ac.in>
2025-03-16 11:46:16 +0530

    This is test

--- test/src/main/java/com/example/test/MainActivity.java
+++ test/src/main/java/com/example/test/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.test;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void display(){
+        Toast.makeText(this, "Sample", Toast.LENGTH_SHORT).show();
     }
 }
